7601939164f0719bb388731380c9a63969913d70
Thibaut LEBRETON <thibaut.lebreton1@etu.univ-lorraine.fr>
2021-01-19 12:45:18 +0100

    Ajout d'un ancien commit.

--- src/main/java/fr/polytech/TPRest/Servlet/HelloServlet.java
+++ src/main/java/fr/polytech/TPRest/Servlet/HelloServlet.java
@@ -1,13 +1,13 @@
 package fr.polytech.TPRest.Servlet;
 
-import com.sun.research.ws.wadl.Response;
-
 import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloServlet extends HttpServlet {
@@ -18,4 +18,35 @@ public class HelloServlet extends HttpServlet {
         return "hello";
     }
 
+
+    @POST
+    @Path("post")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addPerson(Person person) {
+        return Response.ok().entity(person).cookie(new NewCookie("person", person.toString())).build();
+    }
+
+    @GET
+    @Path("get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPerson() {
+        return Response.ok().entity(new Person("Lucas", "Cuoco")).build();
+    }
+
+    @PUT
+    @Path("put")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response putPerson(Person person) {
+        return Response.ok().entity(person).cookie(new NewCookie("person", person.toString())).build();
+    }
+
+    @DELETE
+    @Path("delete")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deletePerson(Person person) {
+        return Response.ok().entity(person).build();
+    }
 }
---
+++ src/main/java/fr/polytech/TPRest/Servlet/Person.java
@@ -0,0 +1,34 @@
+package fr.polytech.TPRest.Servlet;
+
+public class Person {
+    String firstName;
+    String lastName;
+
+    public Person(){}
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person[firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
+}
